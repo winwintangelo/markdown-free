8a3c0a31c42dbb017b93eb6b57fef470218d45a2
winwintangelo <winwintangelo@gmail.com>
2025-12-07 13:36:13 -0600

    Simplify next.config.js - external packages config not needed with self-hosted tar

--- next.config.js
+++ next.config.js
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Required for @sparticuz/chromium-min to work on Vercel
-  // See: https://vercel.com/kb/guide/deploying-puppeteer-with-nextjs-on-vercel
-  experimental: {
-    serverComponentsExternalPackages: ["@sparticuz/chromium-min", "puppeteer-core"],
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
